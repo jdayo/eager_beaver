ac6274748b560feea18dadac653c5bac569f5fbc
jdayo <jdayo@friendster.com>
2013-05-10 10:26:01 +0800

    eager beaver gem filee

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '68cfb88886a0257ca63805730f1f271f1601a112e1fc35d9ccd8a29a93f1573f263a1126123c947b892283385e131b2102856f45527e9b43a9afb4b49bcf10fe'
--- test/eager_beaver_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EagerBeaverTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, EagerBeaver
-  end
-end
